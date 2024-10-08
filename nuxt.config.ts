bf2a9a96cbd4d710df286b39e313d6d0b4b6fedc
mikii17 <michael12gashaw@gmail.com>
2024-10-09 00:05:03 +0300

    feat: add Storybook configuration and modules
    Add Storybook configuration and modules to the project. This includes adding the "@nuxtjs/storybook" and "@nuxtjs/tailwindcss" modules to the Nuxt configuration. The Storybook configuration includes specifying the URL, route, and port for running Storybook.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/storybook", "@nuxtjs/tailwindcss"],
+  storybook: {
+    url: "http://localhost:6006",
+    storybookRoute: "/__storybook__",
+    port: 6006,
+  },
+  // optimizeDeps: { include: ["jsdoc-type-pratt-parser"] },
+});
